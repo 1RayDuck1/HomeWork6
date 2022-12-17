8a6210355623a15c973fb56b6212e4148cb805f8
Duck <kondratev_28@inbox.ru>
2022-12-17 23:21:59 +0300

    Task 1 add

---
+++ HomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿//Домашнее задание.
+
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+int AmountPositivNumbers(int num, int num1, int num2, int num3, int num4)
+{
+    int sum = 0;
+    if(num > 0) sum++;
+    if(num1 > 0) sum++;
+    if(num2 > 0) sum++;
+    if(num3 > 0) sum++;
+    if(num4 > 0) sum++;
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ещё число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ЕЩЁ число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите больше чисел: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ЕЩЁ БОЛЬШЕ ЧИСЕЛ!!: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+int ans = AmountPositivNumbers(num, num1, num2, num3, num4);
+
+Console.WriteLine($"Количество положительных чисел ровна: {ans}.");
+*/
